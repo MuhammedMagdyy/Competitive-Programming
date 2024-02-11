5e7db80a749dfe4f33d1ded08c488bb298d8aff6
MuhammedMagdyy <mohamedmody04@gmail.com>
2024-02-12 00:02:02 +0200

    codeforces modifications

--- Codeforces/Educational Codeforces Round/A - Quasi-palindrome.cpp
+++ Codeforces/Educational Codeforces Round/A - Quasi-palindrome.cpp
@@ -1,36 +1,19 @@
-/*
-    In the name of Allah
-    MeGzz *_*
-    Next Step? ACPC Insha'Allah
-    <I CAN DO IT/>
-*/
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-#define read(FILE)  freopen(FILE, "r", stdin);
-#define write(FILE) freopen(FILE, "w", stdout);
-#define PY puts("YES")
-#define PN puts("NO")
-#define P puts("")
-#define sf scanf
-#define pf printf
-#define pb push_back
-#define endl '\n'
-const int MOD = 1e9 + 7;
 
 int main() {
     int x;
-    sf("%d", &x);
+    cin >> x;
     while(x % 10 == 0) {
         x /= 10;
     }
     string s = to_string(x);
     reverse(s.begin(), s.end());
     if (to_string(x) == s) {
-        PY;
+        cout << "YES" << '\n';
     }
     else {
-        PN;
+        cout << "NO" << '\n';
     }
     return 0;
 }
--- Codeforces/Educational Codeforces Round/B - Kayaking.cpp
+++ Codeforces/Educational Codeforces Round/B - Kayaking.cpp
@@ -1,38 +1,22 @@
-/*
-    In the name of Allah
-    MeGzz *_*
-    Next Step? ACPC Insha'Allah
-    <I CAN DO IT/>
-*/
 #include <bits/stdc++.h>
 using namespace std;
-#define read(FILE)  freopen(FILE, "r", stdin);
-#define write(FILE) freopen(FILE, "w", stdout);
-#define PY puts("YES")
-#define PN puts("NO")
-#define P puts("")
-#define sf scanf
-#define pf printf
-#define pb push_back
-#define endl '\n'
-typedef long long ll;
-const int MOD = 1e9 + 7;
+
 const int MAX = 1005;
 int arr[MAX];
 
 int main() {
     int n, totSum = INT_MAX;
-    sf("%d", &n);
+    cin >> n;
     n *= 2;
     for(int i = 0; i < n; ++i) {
-        sf("%d", &arr[i]);
+        cin >> arr[i];
     }
     for (int i = 0; i < n; ++i) {
         for (int j = i + 1; j < n; ++j) {
             vector<int> v;
             for(int k = 0; k < n; ++k) {
                 if (i != k && j != k) {
-                    v.pb(arr[k]);
+                    v.push_back(arr[k]);
                 }
             }
             sort(v.begin(), v.end());
@@ -43,6 +27,6 @@ int main() {
             totSum = min(totSum, partSum);
         }
     }
-    pf("%d\n", totSum);
+    cout << totSum << '\n';
     return 0;
 }
